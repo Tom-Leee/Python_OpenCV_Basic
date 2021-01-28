b647246b4348354dee334a3ff203b6a279a185fe
Green <joohyoung.t.lee@gmail.com>
2021-01-28 16:10:22 +0900

    21-01-28

--- save_video.py
+++ save_video.py
@@ -1,4 +1,23 @@
 import cv2
+cap = cv2.VideoCapture(0) # 0번 카메라 연결
+if cap.isOpened():
+    while True:
+        ret, frame = cap.read() # 카메라 프레임 일기
+        if ret:
+            cv2.imshow('camera', frame) # 프레임 화면에 표시
+            if cv2.waitKey(1) != -1: # 아무 키나 누르면
+                cv2.imwrite('photo.jpg', frame) # 프레임을 'photo.jpg'에 저장
+                break
+        else:
+            print('no frame')
+            break
+else:
+    print("no camera!")
+
+cap.release()
+cv2.destroyAllWindows()
+
+
 cap = cv2.VideoCapture(0)
 if cap.isOpened():
     file_path = "image_file/recode.avi" #저장할 파일 경로 이름 ①
